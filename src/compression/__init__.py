fdfd577ba84e36e6589ccacba7b86000301a0a6e
Pavan Raj <pvnnraj@gmail.com>
2026-03-03 21:03:16 +0530

    match init and baseline to main

--- src/compression/__init__.py
+++ src/compression/__init__.py
@@ -1,5 +1,5 @@
-# Keep compression package lightweight by default
-# Baseline and advanced compressors should be imported explicitly
+from .quitox_filter import QuitoxCoarseFilter
+from .exit_baseline import ExitBaselineCompressor
+from .hybrid_compressor import HybridCompressor
 
-
-__all__ = ['ExitBaselineCompressor']
+__all__ = ['ExitBaselineCompressor', 'QuitoxCoarseFilter', 'HybridCompressor']
